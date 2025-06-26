d33fd016da31ba768a8cba05d5f9c07c57f060e5
Khubaib Ashraf <118386298+Khubaib3737@users.noreply.github.com>
2025-06-26 23:29:03 +0500

    Update Toxic_Comment.py

--- Toxic Comment Classification/Toxic_Comment.py
+++ Toxic Comment Classification/Toxic_Comment.py
@@ -1,6 +1,5 @@
 
 import pandas as pd
-import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
